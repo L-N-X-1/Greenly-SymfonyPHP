809efa351897134f7afbe8919b9c9e50c5ecbde1
Mohamed Rayen Ben Azouz <123302370+L-N-X-1@users.noreply.github.com>
2025-02-07 10:51:22 +0100

    test2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,6 +11,71 @@ class AdminController extends AbstractController
     #[Route('/admin/dashboard', name: 'admin_dashboard')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        return $this->render('admin/pages/dashboard.html.twig');
+    }
+    #[Route('/admin/tables', name: 'admin_tables')]
+    public function tables(): Response
+    {
+        return $this->render('admin/pages/tables.html.twig');
+    }
+    #[Route('/admin/billing', name: 'admin_billing')]
+    public function billing(): Response
+    {
+        return $this->render('admin/pages/billing.html.twig');
+    }
+    #[Route('/admin/icons', name: 'admin_icons')]
+    public function icons(): Response
+    {
+        return $this->render('admin/pages/icons.html.twig');
+    }
+    #[Route('/admin/landing', name: 'admin_landing')]
+    public function landing(): Response
+    {
+        return $this->render('admin/pages/landing.html.twig');
+    }
+    #[Route('/admin/map', name: 'admin_map')]
+    public function map(): Response
+    {
+        return $this->render('admin/pages/map.html.twig');
+    }
+    #[Route('/admin/notifications', name: 'admin_notifications')]
+    public function notifications(): Response
+    {
+        return $this->render('admin/pages/notifications.html.twig');
+    }
+    #[Route('/admin/profile', name: 'admin_profile')]
+    public function profile(): Response
+    {
+        return $this->render('admin/pages/profile.html.twig');
+    }
+    #[Route('/admin/rtl', name: 'admin_rtl')]
+    public function rtl(): Response
+    {
+        return $this->render('admin/pages/rtl.html.twig');
+    }
+    #[Route('/admin/sign-in', name: 'admin_sign-in')]
+    public function sign(): Response
+    {
+        return $this->render('admin/pages/sign-in.html.twig');
+    }
+    #[Route('/admin/sign-up', name: 'admin_sign-up')]
+    public function up(): Response
+    {
+        return $this->render('admin/pages/sign-up.html.twig');
+    }
+    #[Route('/admin/template', name: 'admin_template')]
+    public function template(): Response
+    {
+        return $this->render('admin/pages/template.html.twig');
+    }
+    #[Route('/admin/typography', name: 'admin_typography')]
+    public function typography(): Response
+    {
+        return $this->render('admin/pages/typography.html.twig');
+    }
+    #[Route('/admin/virtual-reality', name: 'admin_virtual-reality')]
+    public function virtual(): Response
+    {
+        return $this->render('admin/pages/virtual-reality.html.twig');
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,4 +15,27 @@ class MainController extends AbstractController
             'controller_name' => 'Page d\'accueil',
         ]);
     }
+    #[Route('/transactions', name: 'app_transactions')]
+    public function transactions(): Response
+    {
+        return $this->render('main/transactions.html.twig');
+    }
+
+    #[Route('/associations', name: 'app_associations')]
+    public function associations(): Response
+    {
+        return $this->render('main/associations.html.twig');
+    }
+
+    #[Route('/evenements', name: 'app_evenements')]
+    public function evenements(): Response
+    {
+        return $this->render('main/evenements.html.twig');
+    }
+
+    #[Route('/produits', name: 'app_produits')]
+    public function contact(): Response
+    {
+        return $this->render('main/produits.html.twig');
+    }
 }
